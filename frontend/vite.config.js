5c2d9887d47db177452d1a1658ed41a489c0cc1f
rudopulos <79414851+rudopulos@users.noreply.github.com>
2024-04-21 19:00:47 +0300

    v2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/SlimMom/",
+  base: "/SlimMom",
   server: {
     proxy: {
       "/api": "http://localhost:3000",
